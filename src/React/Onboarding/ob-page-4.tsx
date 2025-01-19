b1bb51a6bd7a85eac5f294c18167488bd34d23fe
Harsh Choudhary <choudharyharshofficial@gmail.com>
2025-01-19 14:09:42 +0530

    Ob Pages separated

--- src/React/Onboarding/ob-page-4.tsx
+++ src/React/Onboarding/ob-page-4.tsx
@@ -1,43 +1,10 @@
-import { Button, Group, Paper, Select, Space, Title } from "@mantine/core";
-import { useForm } from "@mantine/form";
+import { Button, Group, Title } from "@mantine/core";
 import React from "react";
 
-const options: SystemMode[] = ["MASTER", "CLIENT"];
-
 const OBPage1 = ({ next, prev }: OBPageProps) => {
-    const form = useForm({
-        initialValues: {
-            mode: "MASTER",
-        },
-    });
-
     return (
         <React.Fragment>
-            <Title ta="center">Let's get started</Title>
-            <Space h="lg"></Space>
-            <Paper p="xl" shadow="lg" mb="md">
-                <form>
-                    <Select
-                        label="System Mode"
-                        data={options}
-                        {...form.getInputProps("mode")}
-                        description={
-                            <React.Fragment>
-                                <ul>
-                                    <li>
-                                        <b>Master:</b> Data loggers/Scanners are <b>Connected</b>{" "}
-                                        here
-                                    </li>
-                                    <li>
-                                        <b>Client:</b> This is a <b>Remote View</b> computer
-                                    </li>
-                                </ul>
-                            </React.Fragment>
-                        }
-                    ></Select>
-                </form>
-                * This cannot be changed later
-            </Paper>
+            <Title ta="center">Page 4</Title>
 
             <Group justify="center" gap="xl">
                 <Button size="md" color="red" onClick={() => prev()}>
